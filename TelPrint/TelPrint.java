150061b722e51d20af3ca420ba1d9b52bdad4485
王以牧 <13116157862@163.com>
2021-04-15 22:45:01 +0800

    电话号码输出

---
+++ TelPrint/TelPrint.java
@@ -0,0 +1,11 @@
+public class TelPrint{
+	public static void main(String[] args){
+		int[] arr = new int[]{8,2,1,0,3};
+		int[] index = new int[]{2,0,3,2,4,0,1,3,2,3,3};
+		String tel =" ";
+		for(int i = 0; i < index.length;i ++){
+			tel += arr[index[i]];
+		}
+		System.out.print("联系方式：" + tel);
+	}
+}
