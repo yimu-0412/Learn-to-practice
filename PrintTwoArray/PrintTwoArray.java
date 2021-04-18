7185b8ecc7983b59c89121b8864d31f6fa17498c
王以牧 <13116157862@163.com>
2021-04-18 16:37:22 +0800

    java 遍历二维数组

---
+++ PrintTwoArray/PrintTwoArray.java
@@ -0,0 +1,11 @@
+public class PrintTwoArray{
+    public static void main(String[] args) {
+        int[][] arr= {{1,2},{1,2,32}};
+        for(int i = 0;i < arr.length;i ++){
+            for(int j = 0; j < arr[i].length;j ++){
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
