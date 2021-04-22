f73b3ceff8da11acee2a62f16e5937abe2c41fcf
王以牧 <13116157862@163.com>
2021-04-22 23:36:46 +0800

    Java null指针作业题练习

---
+++ MyApplication/MyApplication.java
@@ -0,0 +1,13 @@
+package NowCoder;
+class Test {
+	public static void hello() {
+	    System.out.println("hello");
+	}
+}
+public class MyApplication {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Test test=null;
+		test.hello();
+	}
+}
