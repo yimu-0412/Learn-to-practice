d6f3c483211c428e0b8eea329df75a83d955e7c1
王以牧 <13116157862@163.com>
2021-06-22 23:13:31 +0800

    java 单机考试管理软件

---
+++ 附加项目2：单机考试管理软件/2-参考实现/Project04/src/com/atguigu/exam/test/Exam.java
@@ -0,0 +1,26 @@
+package com.atguigu.exam.test;
+
+import com.atguigu.exam.domain.Item;
+import com.atguigu.exam.service.ItemService;
+import com.atguigu.exam.view.ExamView;
+
+public class Exam {
+    public static void main(String[] args) {
+    	//测试一：
+//    	ItemService itemService = new ItemService();
+//    	List<String> list = itemService.readTextFile("Items.txt");
+//    	for(String s : list){
+//    		System.out.println(s);
+//    	}
+    	
+    	//测试二：
+//    	ItemService itemService = new ItemService();
+//    	Item item = itemService.getItem(1);
+//    	System.out.println(item);
+    	
+        ExamView examView = new ExamView();
+
+        examView.enterMainMenu();
+    	
+    }
+}
