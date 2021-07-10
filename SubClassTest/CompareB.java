fd578fd6b8f73e77881efd912ef0a9abd4b46d97
王以牧 <13116157862@163.com>
2021-07-10 23:13:11 +0800

    Java  Java 8中关于接口的改进的学习

---
+++ SubClassTest/CompareB.java
@@ -0,0 +1,7 @@
+package exer2;
+
+public interface CompareB {
+    default void method3(){
+        System.out.println("CompareB：上海");
+    }
+}
---
+++ SubClassTest/SuperClass.java
@@ -0,0 +1,7 @@
+package exer2;
+
+public class SuperClass {
+    public void method3(){
+        System.out.println("SuperClass:北京");
+    }
+}
