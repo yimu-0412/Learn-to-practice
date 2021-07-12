63e4c2325130e18b22e76a3eb2068e9b583c2b38
王以牧 <13116157862@163.com>
2021-07-12 23:02:19 +0800

    java 异常处理：判断程序的输出结果

---
+++ ReturnExceptionDemo/ReturnExceptionDemo.java
@@ -0,0 +1,32 @@
+/*
+*  判断程序的输出 结果
+*/
+
+public class ReturnExceptionDemo {
+    static void methodA() {
+        try {
+            System.out.println("进入方法 A");
+            throw new RuntimeException("制造异常");
+        } finally {
+            System.out.println("用A方法的finally");
+        }
+    }
+    static void methodB() {
+        try {
+            System.out.println("进入方法B");
+            return;
+        } finally {
+            System.out.println("用B方法的finally");
+        }
+    }
+
+    public static void main(String[] args) {
+        try{
+            methodA();
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+
+        methodB();
+    }
+}
