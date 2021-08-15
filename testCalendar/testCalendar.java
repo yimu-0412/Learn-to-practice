028f17cbfe01cab9c6bab7a8741300bf0ed024ef
王以牧 <13116157862@163.com>
2021-08-15 23:44:01 +0800

    java  java.util.Calendar 日历 类

---
+++ testCalendar/testCalendar.java
@@ -0,0 +1,37 @@
+ @Test
+    public void testCalendar(){
+        //1.实例化
+        //方式一：创建其子类（GregorianCalendar）的对象
+        //方式二：调用其静态方法getInstance()
+        Calendar calendar = Calendar.getInstance();
+
+        //2.常用方法
+        //get()
+        int days = calendar.get(Calendar.DAY_OF_MONTH);
+        System.out.println(days);
+        System.out.println(calendar.get(Calendar.DAY_OF_YEAR));
+        System.out.println("======================================");
+
+        //set()
+        //calendar可变性
+        calendar.set(Calendar.DAY_OF_MONTH,22);
+        days = calendar.get(Calendar.DAY_OF_MONTH);
+        System.out.println(days);
+
+        //add()
+        calendar.add(Calendar.DAY_OF_MONTH, 4);
+        days = calendar.get(Calendar.DAY_OF_MONTH);
+        System.out.println(days);
+        System.out.println("======================================");
+
+        //getTime():日历类---> Date
+        Date date = calendar.getTime();
+        System.out.println(date);
+
+        //setTime():Date ---> 日历类
+        Date date1 = new Date();
+        calendar.setTime(date1);
+        days = calendar.get(Calendar.DAY_OF_MONTH);
+        System.out.println(days);
+
+    }
