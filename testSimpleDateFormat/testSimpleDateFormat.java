529fbb616dfb8a8ca5583372539bedd1f921a81f
王以牧 <13116157862@163.com>
2021-08-15 23:47:04 +0800

    java  3. java.text.SimpleDateFormat类

---
+++ testSimpleDateFormat/testSimpleDateFormat.java
@@ -0,0 +1,38 @@
+/*
+    SimpleDateFormat的使用：SimpleDateFormat对日期Date类的格式化和解析
+
+    1.两个操作：
+    1.1 格式化：日期 --->字符串
+    1.2 解析：格式化的逆过程，字符串 ---> 日期
+
+    2.SimpleDateFormat的实例化
+
+     */
+    @Test
+    public void testSimpleDateFormat() throws ParseException {
+        //实例化SimpleDateFormat:使用默认的构造器
+        SimpleDateFormat sdf = new SimpleDateFormat();
+
+        //格式化：日期 --->字符串
+        Date date = new Date();
+        System.out.println(date);
+
+        String format = sdf.format(date);
+        System.out.println(format);
+
+        //解析：格式化的逆过程，字符串 ---> 日期
+        String str = "19-12-18 上午11:43";
+        Date date1 = sdf.parse(str);
+        System.out.println(date1);
+
+        //*************按照指定的方式格式化和解析：调用带参的构造器*****************
+        //SimpleDateFormat sdf1 = new SimpleDateFormat("yyyyy.MMMMM.dd GGG hh:mm aaa");
+        SimpleDateFormat sdf1 = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+        //格式化
+        String format1 = sdf1.format(date);
+        System.out.println(format1);//2019-02-18 11:48:27
+        //解析:要求字符串必须是符合SimpleDateFormat识别的格式(通过构造器参数体现),
+        //否则，抛异常
+        Date date2 = sdf1.parse("2020-02-18 11:48:27");
+        System.out.println(date2);
+    }
