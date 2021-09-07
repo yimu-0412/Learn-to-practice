1409150ab4c02372e1c37c152051a2e58702fd5e
王以牧 <13116157862@163.com>
2021-09-07 23:50:59 +0800

    java 泛型在继承上的体现和通配符

---
+++ GenericTest2/Person.java
@@ -0,0 +1,8 @@
+
+
+/**
+ * @author shkstart
+ * @create 2019 下午 2:42
+ */
+public class Person {
+}
---
+++ GenericTest2/Student.java
@@ -0,0 +1,7 @@
+
+/**
+ * @author shkstart
+ * @create 2019 下午 2:42
+ */
+public class Student extends Person {
+}
